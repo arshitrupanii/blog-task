a804f72ebac002aa83754e796e66135e38adc8fc
arshitrupanii <arshitrupani6@gmail.com>
2026-09-26 18:16:32 +0530

    changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,13 @@ const port = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }
+));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
